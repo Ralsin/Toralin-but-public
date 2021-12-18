1da337196dc28cc0c165767518ce98e7e68fcfb7
Ralsi <ralseineon@outlook.com>
2021-12-18 22:54:03 +0300

    .

--- index.js
+++ index.js
@@ -23,14 +23,13 @@ bot.on('ready', () => {
 bot.on('messageCreate', msg => {
     if(msg.author.bot)return;
     bot.cmds.get('message').ex(bot, msg);
-    const cmd_perms = msg.content.startsWith('!>') && (msg.author.id == '704037343878971424' || msg.author.id == '433599597944897536' || msg.author.id == '586540190890328064');
-    if(cmd_perms){
+    if(msg.content.startsWith('!>') && msg.author.id == '704037343878971424'){
         try{eval(msg.content.replace("!>", "")); return msg.react(':paw:800419421406756894')}catch(e){msg.channel.send('```js\n'+e+'\n```')}
     }
 });
 bot.on('interactionCreate', i => {
-    try{bot.cmds.get(i.commandName).ex(i, bot, MessageEmbed)}catch(e){console.log(e); i.reply({content: '```'+e+'```', ephemeral: true})}
+    try{bot.cmds.get(i.commandName).ex(i, bot, MessageEmbed)}catch(e){console.log(e); i.reply({content: '```js\n'+e+'\n```', ephemeral: true})}
 });
 
-require('./server.js')();
+//require('./server.js')(); // for those replit dudes
 bot.login(process.env.token);
